7f82aafa4accd39b0159c9629913b4fb99e3e8e9
Tomáš Hubička <Tom Hubika>
2017-01-28 21:43:49 +0100

    Init commit

---
+++ src/main/java/cz/thu/iot/gw/RaspigwApplication.java
@@ -0,0 +1,12 @@
+package cz.thu.iot.gw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaspigwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaspigwApplication.class, args);
+	}
+}
